71f19b515536e6d64bdbc784b2b2ae1f697389f4
Eric Masi <eric.masi90@gmail.com>
2021-04-19 15:45:07 -0400

    Adds react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import GlobalLoader from './global/components/GlobalLoader';
 import CustomerView from './features/customer/CustomerView';
 import AdminView from './features/admin/AdminView';
@@ -8,10 +9,23 @@ export default function App() {
   const show = useSelector((state) => state.globalLoader);
 
   return (
-    <>
+    <Router>
       <GlobalLoader show={show} />
-      <CustomerView />
-      <AdminView />
-    </>
+
+      <nav>
+        <Link to="/customer">Customer</Link>
+        <Link to="/admin">Admin</Link>
+      </nav>
+
+      <Switch>
+        <Route exact path="/customer">
+          <CustomerView />
+        </Route>
+
+        <Route exact path="/admin">
+          <AdminView />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
