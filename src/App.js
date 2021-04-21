cd881c02e70b847d67736a8e4f95eee75c53fece
Eric Masi <eric.masi90@gmail.com>
2021-04-21 19:42:48 -0400

    Refactor to move redux selector into consuming component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import GlobalLoader from './global/components/GlobalLoader';
 import CustomerView from './features/customer/CustomerView';
 import AdminView from './features/admin/AdminView';
-import { currentGlobalLoaderState } from './global/components/globalLoaderSlice';
 
 export default function App() {
-  const show = useSelector(currentGlobalLoaderState);
-
   return (
     <Router>
-      <GlobalLoader show={show} />
+      <GlobalLoader />
 
       <nav style={{ padding: '1rem' }}>
         <Link to="/customer" style={{ marginRight: '1rem' }}>
--- src/global/components/GlobalLoader.js
+++ src/global/components/GlobalLoader.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+// Redux store
+import { currentGlobalLoaderState } from './globalLoaderSlice';
+
+// Styles
 import styles from './GlobalLoader.module.css';
+
+// Components
 import Portal from './Portal';
 
 export default function GlobalLoader(props) {
-  const { message = 'Loading...', show = false } = props;
+  const { message = 'Loading...' } = props;
+  const showLoader = useSelector(currentGlobalLoaderState);
 
   return (
-    <>
-      {show && (
-        <Portal className={styles.container}>
+    <Portal>
+      {showLoader && (
+        <div className={styles.container}>
           <div className={styles.loader}>
             <div></div>
             <div></div>
@@ -16,8 +25,8 @@ export default function GlobalLoader(props) {
             <div></div>
           </div>
           <h3 className={styles.heading}>{message}</h3>
-        </Portal>
+        </div>
       )}
-    </>
+    </Portal>
   );
 }
